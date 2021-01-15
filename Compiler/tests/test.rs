c59bf12d326403c91f143cba2c7dcf49d46fe295
vinyllemon <rsbguerra@gmail.com>
2021-01-15 19:21:18 +0000

    Change single test location

---
+++ Compiler/tests/test.rs
@@ -0,0 +1,26 @@
+struct Point2D {
+  x : i32,
+  y : i32
+}
+
+fn add(p:Point2D) -> i32{
+  return p.x + p.y;
+}
+
+fn main(){  
+
+  let x : bool = false;
+  
+  if x || false {
+    let y:i32=12;
+    println!(12);
+  } else {
+    println!(y);
+  }
+  return;
+
+
+//  println!(add(p));
+  return;
+}
+
