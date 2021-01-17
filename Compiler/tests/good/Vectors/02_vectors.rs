fcddc5971e97307b2dbe801ad8e32c76bf451f21
vinyllemon <rsbguerra@gmail.com>
2021-01-17 19:22:34 +0000

    Fix vector tests

--- Compiler/tests/good/Vectors/02_vectors.rs
+++ Compiler/tests/good/Vectors/02_vectors.rs
@@ -4,11 +4,15 @@ fn print_vec(v:Vec<i32>){
     
     while(i<size){
         println!(v[i]);
-        i = i+1
+        i = i+1;
     }
+
+    return;
 }
 
 fn main() {
  let p:Vec<i32> = vec![1,2,4];
  print_vec(p);
+
+ return;
 }
