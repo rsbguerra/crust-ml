95f32dba98691a1626961605cf3a652ab23fb613
vinyllemon <rsbguerra@gmail.com>
2021-01-17 16:53:45 +0000

    Add good vector test

---
+++ Compiler/tests/good/Vectors/01_vectors.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let p:Vec<i32> = vec![1,2,4];
+    println!(p[2]);
+
+    return;
+}
---
+++ Compiler/tests/good/Vectors/02_vectors.rs
@@ -0,0 +1,14 @@
+fn print_vec(v:Vec<i32>){
+    let size:i32 = v.len();
+    let mut i:i32 = 0;
+    
+    while(i<size){
+        println!(v[i]);
+        i = i+1
+    }
+}
+
+fn main() {
+ let p:Vec<i32> = vec![1,2,4];
+ print_vec(p);
+}
