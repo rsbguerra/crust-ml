27bf84cc1e6c884ae28666473024ac375d036369
rsbguerra <rsbguerra@gmail.com>
2021-01-24 22:41:04 +0000

    Organize orinter

--- Compiler/printer_tast.ml
+++ Compiler/printer_tast.ml
@@ -12,9 +12,8 @@ and string_of_typed_decl = function
     (string_of_typed_block body) ^
     (string_of_prust_types t)
 
-and string_of_pairs acc = function
-  | []         -> acc
-  | (id, t):: tl -> string_of_pairs (acc^id^":"^(string_of_prust_types t)^", ") tl
+and string_of_pairs p = 
+  List.fold_left (fun acc (id, t) -> acc ^ id ^ ":" ^ (string_of_prust_types t) ^ ", ") "" p
 
 and string_of_args args = 
   List.fold_left (fun acc (mut, id, t) -> acc ^ (if mut then "mut " else "") ^ id ^ ":" ^ (string_of_prust_types t)^", ") "" args
@@ -23,18 +22,16 @@ and string_of_prust_types = function
   | Tunit -> "Tunit"
   | Ti32  -> "Ti32"
   | Tbool -> "Tbool"
-  | Tstruct s    -> "Tstruct( " ^ s ^ " )"
+  | Tstruct s -> "Tstruct( " ^ s ^ " )"
   | Tvec t -> "Tvec( " ^ (string_of_prust_types t) ^ " )"
   | Tref t -> "Tref( " ^ (string_of_prust_types t) ^ " )"
   | Trefmut t -> "Trefmut( " ^ (string_of_prust_types t) ^ " )"
 
-and string_of_typed_expr_list acc = function
-  | []      -> acc
-  | hd::tl -> (string_of_typed_expr_list (acc^(string_of_typed_expr hd)^", ") tl)
+and string_of_typed_expr_list exprs =
+  List.fold_left (fun acc e -> acc^(string_of_typed_expr exp)^", ") expr
 
-and string_of_struct_pair_list acc = function
-  | []      -> acc
-  | (id, e)::tl -> string_of_struct_pair_list (acc^id^" : "^(string_of_typed_expr e)^", ") tl
+and string_of_struct_pair_list p =
+  List.fold_left (fun acc (id, e) -> acc^id^" : "^(string_of_typed_expr e)^", ") p
 
 and string_of_call_pair_list pair = 
   List.fold_left (fun acc e -> acc ^ ", " ^ (string_of_typed_expr e)) "" pair
@@ -51,8 +48,8 @@ and string_of_typed_expr = function
   | TEstruct_access (e,id,t1,t2)  -> "TEstrc_access(" ^ (string_of_typed_expr e) ^ ", " ^ id ^ ", " ^ (string_of_prust_types t1) ^ (string_of_prust_types t1) ^ ")"
   | TElen (e, t)              -> "TElen(" ^ (string_of_typed_expr e) ^ (string_of_prust_types t)^ ")"
   | TEvec_access(e1, e2, tid) -> "TEvec_access(" ^(string_of_typed_expr e1)^", "^(string_of_typed_expr e1)^", "^(string_of_prust_types tid)^")"
-  | TEcall (f, el, t)            -> "TEcall("^ f ^ ", " ^ (string_of_call_pair_list el) ^ "," ^(string_of_prust_types t)^")"
-  | TEvec_decl (els, t)          -> "TEvec_decl("^ (List.fold_left(fun a e -> a ^ ", " ^ (string_of_typed_expr e)) "" els) ^ ", " ^ (string_of_prust_types t)^ ")"
+  | TEcall (f, el, t)         -> "TEcall("^ f ^ ", " ^ (string_of_call_pair_list el) ^ "," ^(string_of_prust_types t)^")"
+  | TEvec_decl (els, t)       -> "TEvec_decl("^ (List.fold_left(fun a e -> a ^ ", " ^ (string_of_typed_expr e)) "" els) ^ ", " ^ (string_of_prust_types t)^ ")"
   | TEprint (s, t)  -> "TEprint(" ^ s ^ ", " ^ (string_of_prust_types t) ^ ")"
   | TEblock (bl, t) -> "TEblock(" ^ (string_of_typed_block bl) ^ ", " ^ (string_of_prust_types t) ^ ")"
 
@@ -66,7 +63,7 @@ and string_of_typed_block (stmts, exp, t) =
 and string_of_typed_stmt = function
   | TSnothing t            -> "TSnothing"^(string_of_prust_types t)^")"
   | TSexpr (e,t)           -> "TSexpr("^(string_of_typed_expr e)^", "^(string_of_prust_types t)^")"
-  | TSdeclare(mut,id,e,t) -> "TSdeclare(" ^ (if mut then "mut " else "") ^id^", "^(string_of_typed_expr e)^","^(string_of_prust_types t)^")"
+  | TSdeclare(mut,id,e,t)  -> "TSdeclare(" ^ (if mut then "mut " else "") ^id^", "^(string_of_typed_expr e)^","^(string_of_prust_types t)^")"
   | TSdeclare_struct(mut, id, tid, el, t) -> 
   "TSdeclare_struct(" ^ (if mut then "mut " else "") ^ id^", "^tid^", "^
   (string_of_struct_decl_pair_list el) ^ ", "^
@@ -74,8 +71,7 @@ and string_of_typed_stmt = function
   | TSwhile(e, bl, t)     -> "TSwhile("^(string_of_typed_expr e)^"\n"^(string_of_typed_block bl) ^ ", " ^(string_of_prust_types t)^")"
   | TSreturn (None, t)    -> "TSreturn("^(string_of_prust_types t)^")"
   | TSreturn (Some e, t)  -> "TSreturn("^(string_of_typed_expr e)^", "^(string_of_prust_types t)^")"
-  | TSif (e, bl1, bl2, t) -> 
-  "TSif("^(string_of_typed_expr e)^", "^(string_of_typed_block bl1)^", "^(string_of_typed_block bl2)^", "^(string_of_prust_types t)^")"
+  | TSif (e, bl1, bl2, t) -> "TSif("^(string_of_typed_expr e)^", "^(string_of_typed_block bl1)^", "^(string_of_typed_block bl2)^", "^(string_of_prust_types t)^")"
 
 let print_file s = 
   Printf.printf "TYPED AST:\n\n%s\n\n" (string_of_program s)
